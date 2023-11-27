397c84d0cecf0dc774a727f89f24fdb7ae01c2a8
MariiaSam <kontragent@ukr.net>
2023-11-27 21:47:25 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { FaTrash } from 'react-icons/fa';
+// import { Collapsible } from 'components/Collapsible/Collapsible';
+// import { Container } from './App.styled';
+// import { Tooltip } from 'components/Tooltip/Tooltip';
+import { DropdownMenu } from 'components/DropdownMenu/DropdownMenu';
+import { DropdownMenuItem } from 'components/DropdownMenu/DropdownMenuItem';
+// import { VideoList } from 'components/VideoList/VideoList';
+// import videos from '../../videos.json';
+
+export const App = ()=> {
+return (
+<div>
+  
+<DropdownMenu>
+          <DropdownMenuItem onSelect={() => alert('Delete')}>
+            <FaTrash size={16} color="blue" />
+            Delete
+          </DropdownMenuItem>
+          <DropdownMenuItem onSelect={() => alert('Edit')}>Edit</DropdownMenuItem>
+        </DropdownMenu>
+</div>
+    )
 };
---
+++ src/components/DropdownMenu/Dropdown.js
@@ -0,0 +1,18 @@
+const { Component } = require("react");
+
+
+class Dropdown extends Component {
+render() {
+
+<div className="Dropdown">
+    <button type="button" className="Dropdown__toggle" >
+        Open
+    </button>
+
+    <div className="Dropdown__menu">Menu</div>
+</div>}
+
+
+}
+
+export default Dropdown
