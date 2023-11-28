87646c1e5dde63e168b7c6501539b81fbbb51d17
MariiaSam <kontragent@ukr.net>
2023-11-28 12:17:04 +0200

    ToDoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-import { FaTrash } from 'react-icons/fa';
+// import { FaTrash } from 'react-icons/fa';
 
-import { DropdownMenu } from './DropdownMenu/DropdownMenu'
-import { DropdownMenuItem } from './DropdownMenu/DropdownMenuItem'
+// import { DropdownMenu } from '../components/DropdownMenu/DropdownMenu'
+// import { DropdownMenuItem } from '../components/DropdownMenu/DropdownMenuItem'
+
+import TodoList from './TodoList/TodoList';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 'id_1', text: 'Learn', completed: false },
+      { id: 'id_2', text: 'Counter', completed: false },
+      { id: 'id_3', text: 'Redux', completed: false },
+    ],
+  };
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        <DropdownMenu> 
+        {/* <DropdownMenu> 
           <DropdownMenuItem onSelect={() => alert('Delete')}>
             <FaTrash size={16} color="blue" />
             Delete
           </DropdownMenuItem>
           <DropdownMenuItem onSelect={() => alert('Edit')}>Edit</DropdownMenuItem>
-        </DropdownMenu>
+        </DropdownMenu> */}
+
+        <TodoList todos={todos} />
       </div>
     );
   }
---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,14 @@
+import './TodoList.css'
+
+const TodoList  = ({ todos }) => (
+  <ul className='TodoList'>
+    {todos.map(({id, text, completed}) => (
+      <li key={id} className='TodoList__item'>
+        <p className='TodoList__text'>{text}</p>
+        <button>Delete</button>
+      </li> 
+    ))}
+  </ul>
+);
+
+export default TodoList;
